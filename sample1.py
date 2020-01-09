21dd2ba9ea30e8883f392499f4e7a0122d1f2d3c
krithikap39 <krithikap39@users.noreply.github.com>
2020-01-09 10:55:57 +0530

    Added sample python program for jenkins build

---
+++ sample1.py
@@ -0,0 +1,15 @@
+
+def calculate_iterations():
+
+    number =1; iteration = 0
+
+    while number <= 10000:
+        number = number * 2
+        iteration+=1
+    print("Number is: "+str(number))
+    return iteration
+
+
+if __name__ == '__main__':
+    iteration = calculate_iterations()
+    print("Number of iterations: "+str(iteration))
